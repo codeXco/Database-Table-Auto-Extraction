6ee72a14a21d1fb1bb20201b6512ae3f1d186e14
CodexCo <billcain6@gmail.com>
2015-07-23 12:01:44 +0100

    Update config.php

--- config.php
+++ config.php
@@ -4,8 +4,8 @@ session_start(); // Start Named Session
 
 define( 'DB_HOST', 'localhost' ); // Set the database host (usually localhost)
 define( 'DB_USER', 'root' ); // Set the Database Username (The one you use to connect)
-define( 'DB_PASS', 'cracked' ); // Set the Database Password
-define( 'DB_NAME', 'webcodexcms' ); // Set the Database Name (The database your using)
+define( 'DB_PASS', '' ); // Set the Database Password
+define( 'DB_NAME', '' ); // Set the Database Name (The database your using)
 
 // The site root
 define('DOCROOT', realpath(dirname(__FILE__)).DIRECTORY_SEPARATOR);
