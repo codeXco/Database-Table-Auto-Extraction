0f11436aeed3105094ca9ea50efb8836c8aca660
CodexCo <billcain6@gmail.com>
2015-07-23 16:18:26 +0100

    Update output.php

--- output.php
+++ output.php
@@ -1,20 +1,3 @@
-<?php require_once('config.php'); ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Database Table Auto Extraction - Table Data</title>
-<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-<script src="//cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
-<script>
-$(document).ready(function() {
-    $('#data').DataTable();
-} );
-</script>
-<link href="css/datatable.css" rel="stylesheet" type="text/css" />
-</head>
-
-<body>
 <table id="data" class="display" cellspacing="0" width="100%">
 <?php
 if(isset($_GET['table']) && in_array($_GET['table'], $phpCore->getAllDbTables())) {
@@ -46,5 +29,3 @@ else {
 }
 ?>
 </table>
-</body>
-</html>
