164399e7431d2e2c32677eb147e782391cced93e
CodexCo <billcain6@gmail.com>
2015-07-23 16:17:18 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,25 @@
-<?php require_once('config.php'); ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Database Table Auto Extraction - Table Select</title>
-<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-</head>
+<?php
+session_name('phpCore'); // Session Name
+session_start(); // Start Named Session
 
-<body>
-<?php 
-echo 'Database Name:&nbsp;'.DB_NAME.'<br /><br />';
-echo 'Available Tables:&nbsp;&nbsp;'; 
-foreach($phpCore->getAllDbTables() as $tablenameHeader) {
-	echo '<br /><a href="output.php?table='.$tablenameHeader.'">'.ucfirst($tablenameHeader).'</a>';
+// Autoload the classes in the phpCoreClasses Folder
+spl_autoload_register(function ($class) {
+	require_once 'phpCoreClasses/class.'. $class .'.php';
+});
+
+// Instantiate the class
+$phpCore = new phpCore();
+
+$pagearray = array('tablelist','output');
+
+include_once('header.php');
+
+if(isset($_GET['table']) && !empty($_GET['table']) && in_array($_GET['table'], $phpCore->getAllDbTables())) {
+	include_once('output.php');
 }
-?>
+else {
+	include_once('tablelist.php');
+}
 
-</body>
-</html>
+include_once('footer.php');
+?>
